460bff4fc8a3e722b6cb636cbe80725874a8a94e
dovid <lomedlomed@hotmail.com>
2020-12-09 16:59:47 +0200

    fix for jdn

--- blocker.js
+++ blocker.js
@@ -1,6 +1,7 @@
 var selector = "div[id^='divB'], div[id^='advads'], .jdn-pirsum, script[type$='-text/javascript'], script[src='https://www.jdn.co.il/wp-content/plugins/jdn_ads/js/info.js']";
 var query = document.querySelectorAll(selector);
 
+
 query.forEach(function (element) {
   element.remove()
 });
@@ -11,11 +12,13 @@ query.forEach(function (element) {
 var divsPlayer = document.querySelectorAll(".flowplayer");
 
 for (let player of divsPlayer) 
-  if(player['data-item']){
-      var props = JSON.parse(player['data-item']).source[0];
+  if(player.dataset.item){
+      var props = JSON.parse(player.dataset.item).sources[0];
       var newV = document.createElement('video');
+      newV.classList.add("flowplayer");
       newV.src = props.src;
       newV.type = props.type;
+      newV.controls = true;
       player.replaceWith(newV);
   }
 
