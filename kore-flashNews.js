de2a34c4cd474f1dd0f11e87eb5c6b9eb69859ef
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2024-09-26 02:45:46 +0300

    Update kore-flashNews.js

--- kore-flashNews.js
+++ kore-flashNews.js
@@ -6,33 +6,27 @@ const markAllAsRead = (latestNewsId) => {
   localStorage.setItem(LAST_NEWS_ID_STORAGE_KEY, latestNewsId);
 }
 
-const markNewItems = (containers, lastReadId) => {
+const markNewItems = (lastReadId) => {
   let newItemsCount = 0;
-  let latestNewsId;
+  const latestNewsId = document.querySelector('.newsflash_list .item');
 
-  containers.forEach(container => {
-    for (const newsItem of container.children) {
-      latestNewsId = newsItem.id;
-      if (lastReadId && newsItem.id <= lastReadId) break;
+  document.querySelectorAll('.newsflash_list .item').forEach(newsItem => {
+      if (lastReadId && newsItem.id <= lastReadId) return;
       newsItem.classList.add(NEW_ITEM_CLASS);
       newItemsCount++;
-    }
   });
 
   return { newItemsCount, latestNewsId };
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  const { newItemsCount, latestNewsId } = markNewItems(
-    document.querySelectorAll('.newsflash_list'),
-    localStorage.getItem(LAST_NEWS_ID_STORAGE_KEY)
-  );
+  const { newItemsCount } = markNewItems(localStorage.getItem(LAST_NEWS_ID_STORAGE_KEY));
 
-  setTimeout(() => markAllAsRead(latestNewsId), Math.min(newItemsCount, 5) * 3000 || 5000);
+  setTimeout(markAllAsRead), Math.min(newItemsCount, 5) * 3000 || 5000);
 
   const markAllAsReadButton = document.createElement('div');
   markAllAsReadButton.textContent = 'סמן הכל כנקרא';
   markAllAsReadButton.style.cursor = 'pointer';
-  markAllAsReadButton.addEventListener('click', () => markAllAsRead(latestNewsId));
+  markAllAsReadButton.addEventListener('click', markAllAsRead);
   document.querySelector('.inner_sec_title').after(markAllAsReadButton);
 });
