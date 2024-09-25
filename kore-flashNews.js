b398490cd5e622648b73e997bb5a63d0ad78f728
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2024-09-26 02:49:34 +0300

    Update kore-flashNews.js

--- kore-flashNews.js
+++ kore-flashNews.js
@@ -2,25 +2,24 @@ const LAST_NEWS_ID_STORAGE_KEY = 'lastNewsID';
 const NEW_ITEM_CLASS = 'unread';
 
 const markAllAsRead = (latestNewsId) => {
-  document.querySelectorAll(`.${NEW_ITEM_CLASS}`).forEach(el => el.classList.remove(NEW_ITEM_CLASS));
+  document.querySelectorAll(`.newsflash_list .item.${NEW_ITEM_CLASS}`).forEach(el => el.classList.remove(NEW_ITEM_CLASS));
+  const latestNewsId = document.querySelector('.newsflash_list .item');
   localStorage.setItem(LAST_NEWS_ID_STORAGE_KEY, latestNewsId);
 }
 
 const markNewItems = (lastReadId) => {
   let newItemsCount = 0;
-  const latestNewsId = document.querySelector('.newsflash_list .item');
-
   document.querySelectorAll('.newsflash_list .item').forEach(newsItem => {
       if (lastReadId && newsItem.id <= lastReadId) return;
       newsItem.classList.add(NEW_ITEM_CLASS);
       newItemsCount++;
   });
 
-  return { newItemsCount, latestNewsId };
+  return { newItemsCount };
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  const { newItemsCount } = markNewItems(localStorage.getItem(LAST_NEWS_ID_STORAGE_KEY));
+  const { newItemsCount } = markNewItems(localStorage.getItem(LAST_NEWS_ID_STORAGE_KEY) || 0);
 
   setTimeout(markAllAsRead), Math.min(newItemsCount, 5) * 3000 || 5000);
 
