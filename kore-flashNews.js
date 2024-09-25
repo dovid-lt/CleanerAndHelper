26eac49e6abeffb21a685e8d411cbf3e2c37d604
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2024-09-26 02:30:20 +0300

    Update kore-flashNews.js

--- kore-flashNews.js
+++ kore-flashNews.js
@@ -1,6 +1,7 @@
 const LAST_NEWS_ID_STORAGE_KEY = 'lastNewsID';
-const NEW_ITEM_CLASS = 'new-item';
+const NEW_ITEM_CLASS = 'unread';
 
+const markAllAsRead = () => document.querySelectorAll(`.${NEW_ITEM_CLASS}`).forEach(el => el.classList.remove(NEW_ITEM_CLASS));
 const markNewItems = (containers, lastReadId) => {
   let newItemsCount = 0;
   let latestNewsId;
@@ -25,6 +26,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   setTimeout(() => {
     localStorage.setItem(LAST_NEWS_ID_STORAGE_KEY, latestNewsId);
-    document.querySelectorAll(`.${NEW_ITEM_CLASS}`).forEach(el => el.classList.remove(NEW_ITEM_CLASS));
+    markAllAsRead()
   }, Math.min(newItemsCount, 5) * 3000 || 5000);
 });
+
+$('.inner_sec_title').after(
+  $('<div>סמן הכל כנקרא</div>')
+    .on('click', markAllAsRead)
+    .css('cursor', 'pointer')
+);
