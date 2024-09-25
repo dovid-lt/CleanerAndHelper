b24630b8694c464c2b0a2314cd07598a48441834
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2024-09-26 02:14:10 +0300

    Update kore-flashNews.js

--- kore-flashNews.js
+++ kore-flashNews.js
@@ -1,47 +1,5 @@
-const key = 'lastNews';
+const key = 'lastNewsID';
 const lastRead = localStorage.getItem(key);
-
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('newsFlash')?.remove();
-
-  let container = document.querySelector('.newsflash_list');
-  let it = container.children[Symbol.iterator]()
-  let last = null;
-
-  let result = it.next();
-  if (!result.done)
-    last = result.value.id;
-
-  let currDate = Date.now();
-  let counter = 0;
-
-  if (lastRead) {
-    container.classList.add('ext-c');
-    do {
-      if (result.value.id <= lastRead) break;
-      counter++;
-      result.value.classList.add('new-item');
-      humanDate(result.value.children[0], currDate);
-
-      result = it.next();
-    } while (!result.done)
-  }
-
-  setTimeout(() => {
-    localStorage.setItem(key, last)
-    document.querySelectorAll('.new-item').forEach(el => el.classList.remove('new-item'));
-  }, counter ? Math.min(counter, 5) * 3000 : 5000);
-});
-
-function humanDate(el, currDate) {
-  let reg = el?.innerText?.match(/(\d+)\.(\d+)\.(\d+)\ (\d\d):(\d\d)/);
-  if (reg) {
-    let extDate = new Date(reg[3], reg[2] - 1, reg[1], reg[4], reg[5]);
-    let rel = timeDifference(currDate, extDate);
-    el.innerText += `  (${rel})`;
-  }
-}
-
 
 const rtf = new Intl.RelativeTimeFormat('he', { numeric: "auto" });
 const msPerMinute = 60 * 1000;
@@ -62,3 +20,33 @@ function timeDifference(baseDate, theDate) {
   else
     return rtf.format(-Math.floor(elapsed / msPerDay), 'day');
 }
+
+function humanDate(element, now) {
+  const date = new Date(element.getAttribute('datetime'));
+  element.textContent = timeDifference(now, date);
+}
+
+document.addEventListener('DOMContentLoaded', function () {  
+  let containers = document.querySelectorAll('.newsflash_list');
+  let counter = 0;
+  let last;
+  
+  containers.forEach(container => {
+    const items = container.children;
+    for (const item of items) {
+      last = item.id;
+      if (lastRead) {
+        if (item.id <= lastRead) break;
+        counter++;
+        item.classList.add('new-item');
+        let now = Date.now();
+        humanDate(item.children[0], now);
+      }
+    }
+  });
+
+  setTimeout(() => {
+    localStorage.setItem(key, last);
+    document.querySelectorAll('.new-item').forEach(el => el.classList.remove('new-item'));
+  }, counter ? Math.min(counter, 5) * 3000 : 5000);
+});
